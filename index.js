ce340f0529cc1c00a14eb14cd68f50d26ef16481
griffincregan <griffinregan814@gmail.com>
2022-05-18 20:09:32 -0500

    commit tree 8f22be9b03d2d4f8b3cc17841e168f764e6500da

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+var multiply = num1 * num2
+
+
+
+
+var random = Math.ceil(Math.random());
+
+const num3 = 4
+const num4 = 12
+
+var mod = num3 % num4
+
+var max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
